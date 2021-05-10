78853307598b30adef372125d5fba50e066ae1d6
Moamen236 <Moamen236@users.noreply.github.com>
2021-05-10 14:31:58 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -10,43 +10,45 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 class App extends Component {
-  state = { 
-    movies:[],
-    tv:[]
-   }
-
-  getTrending = async (mediaType) =>{
-    let {data} = await axios.get(`http://api.themoviedb.org/3/trending/${mediaType}/week?api_key=f1aca93e54807386df3f6972a5c33b50`)
-    // console.log(data.results)
-    this.setState({[mediaType]:data.results})
+  state = {
+    movies: [],
+    tv: []
   }
 
-  componentDidMount(){
+  getTrending = async (mediaType) => {
+    let { data } = await axios.get(`http://api.themoviedb.org/3/trending/${mediaType}/week?api_key=f1aca93e54807386df3f6972a5c33b50`)
+    // console.log(data.results)
+    this.setState({ [mediaType]: data.results })
+  }
+
+  componentDidMount() {
     this.getTrending('movies');
     this.getTrending('tv')
 
   }
 
-  
-  render() { 
-    return ( 
+
+  render() {
+    return (
       <>
-      <Navbar></Navbar>
-      <div className="container">
-        <Switch>
-          <Route path="/home" render={ (props)=> <Home {...props} trendingMovies={this.state.movies}  trendingTv={this.state.tv}/> }></Route>
-          <Route path="/movise" render={(props)=> <Movise {...props} trendingMovies={this.state.movies}/>} ></Route>
-          <Route path="/tv" render={(props)=> <Tv {...props} trendingTv={this.state.tv}/>}></Route>
-          <Route path="/notfound" component={Notfound}></Route>
-          <Redirect from="/" exact to="/home"></Redirect>
-          <Redirect to="/notfound"></Redirect>
-          
-        </Switch>
-      </div>
+        <Navbar></Navbar>
+        <div className="container">
+          <Switch>
+            <Route path="/home" render={(props) => <Home {...props} trendingMovies={this.state.movies} trendingTv={this.state.tv} />}></Route>
+            <Route path="/movise" render={(props) => <Movise {...props} trendingMovies={this.state.movies} />} ></Route>
+            <Route path="/tv" render={(props) => <Tv {...props} trendingTv={this.state.tv} />}></Route>
+            <Route path="/notfound" component={Notfound}></Route>
+            <Redirect from="/" exact to="/home"></Redirect>
+            <Redirect to="/notfound"></Redirect>
+
+          </Switch>
+        </div>
       </>
-    
-     );
+
+    );
   }
 }
- 
+
 export default App;
+
+// comment
